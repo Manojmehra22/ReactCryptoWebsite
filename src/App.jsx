de5634876c9a7af25a4492f4b45896a3cfc7507e
Manojmehra22 <manojmehr650@gmail.com>
2024-10-29 23:27:27 +0530

    new project added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
